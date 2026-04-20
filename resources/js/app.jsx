07fc257f1a63110caa6678478c369adde1b499c0
citrapaulia0-ai <citrapaulia0@gmail.com>
2026-04-20 16:33:01 +0700

    c

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,30 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import KelolaProduk from "./Pages/KelolaProduk";
-import Login from "./Pages/Login";
-import Dashboard from "./Pages/Dashboard";
-
-function App() {
-  return (
-    <Router>
-
-      {window.location.pathname !== "/" && <Navbar role="" />}
-
-      <div className="p-10">
-
-        <Routes>
-
-          <Route path="/" element={<Login />} />
-
-          <Route path="/dashboard" element={<Dashboard />} />
-
-          <Route path="/kelola-produk" element={<KelolaProduk />} />
-
-        </Routes>
-
-      </div>
-    </Router>
-  );
 import KelolaKategori from "./Pages/KelolaKategori";
 import ProfilPengguna from "./Pages/ProfilPengguna";
 import EditProfil from "./Pages/EditProfil";
@@ -34,7 +10,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<KelolaKategori />} />
                 <Route path="/kelola-produk" element={<KelolaProduk />} />
-                <Route path="/kelola-kategori" element={<KelolaKategori />} />
+                <Route path="/kelola-kategori" element={<KelolaKategori/>} />
                 <Route path="/profil-pengguna" element={<ProfilPengguna />} />
                 <Route path="/edit-profil" element={<EditProfil />} />
             </Routes>
@@ -42,4 +18,4 @@ function App() {
     );
 }
 
-export default App;}
+export default App;
