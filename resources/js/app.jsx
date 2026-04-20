f390d34f72349ec3b6b28ec1797641dc9ec32329
ftzahra <fatiahfatiah39@gmail.com>
2026-04-20 09:52:55 +0700

    Menambah routes login

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,45 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import KelolaProduk from "./Pages/KelolaProduk";
-import Login from "./Pages/Login";
-import Dashboard from "./Pages/Dashboard";
-
-function App() {
-  return (
-    <Router>
-
-      {window.location.pathname !== "/" && <Navbar role="" />}
-
-      <div className="p-10">
-
-        <Routes>
-
-          <Route path="/" element={<Login />} />
-
-          <Route path="/dashboard" element={<Dashboard />} />
-
-          <Route path="/kelola-produk" element={<KelolaProduk />} />
-
-        </Routes>
-
-      </div>
-    </Router>
-  );
 import KelolaKategori from "./Pages/KelolaKategori";
-import ProfilPengguna from "./Pages/ProfilPengguna";
-import EditProfil from "./Pages/EditProfil";
+import Login from "./Pages/Login";
+
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<KelolaKategori />} />
+                <Route path="/kelola-kategori" element={<KelolaKategori />} />
                 <Route path="/kelola-produk" element={<KelolaProduk />} />
                 <Route path="/kelola-kategori" element={<KelolaKategori />} />
-                <Route path="/profil-pengguna" element={<ProfilPengguna />} />
-                <Route path="/edit-profil" element={<EditProfil />} />
+                <Route path="/login" element={<Login />} />
             </Routes>
         </Router>
     );
 }
 
-export default App;}
+export default App;
