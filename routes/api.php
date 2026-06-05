365400c63d48a474f5f1c755913de0b6355272ed
Fathia Zahra <153520624+ftzahra@users.noreply.github.com>
2026-06-05 10:31:39 +0700

    menambah route backend page produk

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\ProdukController;
 
 Route::get('/kategori', [KategoriController::class, 'index']);
 Route::post('/kategori', [KategoriController::class, 'store']);
 Route::put('/kategori/{id}', [KategoriController::class, 'update']);
 Route::delete('/kategori/{id}', [KategoriController::class, 'destroy']);
+Route::resource('produk', ProdukController::class);
