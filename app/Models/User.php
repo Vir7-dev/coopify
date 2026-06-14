7f92ebea801a2198109957cc5fefa57e4ff741be
windawindaa03 <winda.windaa03@gmail.com>
2026-06-14 22:54:42 +0700

    Memisahkan icon kategori ke constants/categoryIcons, tambah SweetAlert2 untuk notifikasi CRUD Kategori, dan hapus warna badge jumlah produk

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,30 +2,45 @@
 
 namespace App\Models;
 
+use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable; 
 
-    protected $table = 'pengguna';
-
+    protected $table      = 'pengguna';
     protected $primaryKey = 'id_pengguna';
+    public    $timestamps = false;
 
     protected $fillable = [
         'nim_nik',
         'nama',
-        'password',
-        'foto_profl',
+        'email',
         'no_hp',
+        'foto_profil',
         'role',
-    ];
-
-    protected $hidden = [
         'password',
     ];
 
-    public $timestamps = false;
+    protected $hidden = ['password'];
+
+    protected function casts(): array
+    {
+        return [
+            'password' => 'hashed',
+        ];
+    }
+
+    public function getNameAttribute()
+    {
+        return $this->nama;
+    }
+
+    public function username()
+    {
+        return 'nim_nik';
+    }
 }
