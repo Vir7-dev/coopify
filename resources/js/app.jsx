42858bc55724e82d9fa1ba35c21e60aa486b05ed
windawindaa03 <winda.windaa03@gmail.com>
2026-06-15 00:32:41 +0700

    Tambah route profil admin dan edit profil, serta hapus import Search di app.jsx

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -11,30 +11,26 @@ import EditProfil from "./Pages/EditProfil";
 import Produk from "./Pages/Produk";
 import Login from "./Pages/Login";
 import DetailProduk from "./Pages/DetailProduk";
-import Search from "./Pages/Search";
 import DashboardAdmin from "./Pages/DashboardAdmin";
 import Dashboard from "./Pages/Dashboard";
 
 function App() {
     return (
         <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/dashboard-pengguna" element={<Dashboard />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/produk/:kategori" element={<Produk />} />
-            <Route path="/kelola-produk" element={<KelolaProduk />} />
-            <Route path="/kelola-kategori" element={<KelolaKategori />} />
-            <Route path="/keranjang" element={<Keranjang />} />
-            <Route path="/pembayaran" element={<Pembayaran />} />
-            <Route path="/profil-pengguna" element={<ProfilPengguna />} />
-            <Route path="/profil-admin" element={<ProfilAdmin />} />
-            <Route path="/edit-profil" element={<EditProfil />} />
-            <Route path="/produk" element={<Produk />} />
-            <Route path="/dashboard-admin" element={<DashboardAdmin />} />
-
-
-
-
+            <Route path="/"                   element={<Dashboard />}      />
+            <Route path="/dashboard-pengguna" element={<Dashboard />}      />
+            <Route path="/dashboard-admin"    element={<DashboardAdmin />} />
+            <Route path="/login"              element={<Login />}          />
+            <Route path="/produk"             element={<Produk />}         />
+            <Route path="/produk/:kategori"   element={<Produk />}         />
+            <Route path="/produk/detail/:id"  element={<DetailProduk />}   />
+            <Route path="/kelola-produk"      element={<KelolaProduk />}   />
+            <Route path="/kelola-kategori"    element={<KelolaKategori />} />
+            <Route path="/keranjang"          element={<Keranjang />}      />
+            <Route path="/pembayaran"         element={<Pembayaran />}     />
+            <Route path="/profil-pengguna"    element={<ProfilPengguna />} />
+            <Route path="/profil-admin"       element={<ProfilAdmin />}    />
+            <Route path="/edit-profil"        element={<EditProfil />}     />
         </Routes>
     );
 }
